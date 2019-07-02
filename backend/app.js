5dd2f055fe7c6ebda3132c83935878442738480b
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2019-07-02 14:39:36 +0800

    frontend and api connections

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,17 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPtIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+})
+
 app.use(isAuth);
 
 app.use(
@@ -30,7 +41,7 @@ mongoose
     }?retryWrites=true&w=majority`
   , { useNewUrlParser: true })
   .then(() => {
-    app.listen(3000);
+    app.listen(3001);
   })
   .catch(err => {
     console.log(err);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-
+import MainNavigation from './components/Navigation/MainNavigation';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Booking';
 import EventsPage from './pages/Events';
@@ -10,12 +10,17 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+      <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/events" component={EventsPage} />
+            <Route path="/bookings" component={BookingsPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
